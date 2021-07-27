13ce917032e5b047685c1c18547d7e1a5c54c44d
Thomas Mueller <mueller@adobe.com>
2021-07-27 22:50:35 +0200

    Tweak performance counters

--- benchmarks/linux-perf-events.h
+++ benchmarks/linux-perf-events.h
@@ -6,7 +6,7 @@
 #include <linux/perf_event.h> // for perf event constants
 #include <sys/ioctl.h>        // for ioctl
 #include <unistd.h>           // for syscall
-#include <iostream>
+
 #include <cerrno>  // for errno
 #include <cstring> // for memset
 #include <stdexcept>
@@ -14,15 +14,14 @@
 #include <vector>
 
 template <int TYPE = PERF_TYPE_HARDWARE> class LinuxEvents {
-  int group;
-  bool working;
+  int fd;
   perf_event_attr attribs;
   int num_events;
   std::vector<uint64_t> temp_result_vec;
-  std::vector<int> fds;
+  std::vector<uint64_t> ids;
 
 public:
-  explicit LinuxEvents(std::vector<int> config_vec) : group(-1), working(true) {
+  LinuxEvents(std::vector<int> config_vec) : fd(0) {
     memset(&attribs, 0, sizeof(attribs));
     attribs.type = TYPE;
     attribs.size = sizeof(attribs);
@@ -36,16 +35,16 @@ public:
     const int cpu = -1; // all CPUs
     const unsigned long flags = 0;
 
+    int group = -1; // no group
     num_events = config_vec.size();
     uint32_t i = 0;
     for (auto config : config_vec) {
       attribs.config = config;
-      int fd = syscall(__NR_perf_event_open, &attribs, pid, cpu, group, flags);
+      fd = syscall(__NR_perf_event_open, &attribs, pid, cpu, group, flags);
       if (fd == -1) {
         report_error("perf_event_open");
       }
-
-      fds.push_back(fd);
+      ioctl(fd, PERF_EVENT_IOC_ID, &ids[i++]);
       if (group == -1) {
         group = fd;
       }
@@ -54,28 +53,24 @@ public:
     temp_result_vec.resize(num_events * 2 + 1);
   }
 
-  ~LinuxEvents() {
-    for (auto fd : fds) {
-      close(fd);
-    }
-  }
+  ~LinuxEvents() { close(fd); }
 
   inline void start() {
-    if (ioctl(group, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP) == -1) {
+    if (ioctl(fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP) == -1) {
       report_error("ioctl(PERF_EVENT_IOC_RESET)");
     }
 
-    if (ioctl(group, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP) == -1) {
+    if (ioctl(fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP) == -1) {
       report_error("ioctl(PERF_EVENT_IOC_ENABLE)");
     }
   }
 
   inline void end(std::vector<unsigned long long> &results) {
-    if (ioctl(group, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP) == -1) {
+    if (ioctl(fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP) == -1) {
       report_error("ioctl(PERF_EVENT_IOC_DISABLE)");
     }
 
-    if (read(group, &temp_result_vec[0], temp_result_vec.size() * 8) == -1) {
+    if (read(fd, &temp_result_vec[0], temp_result_vec.size() * 8) == -1) {
       report_error("read");
     }
     // our actual results are in slots 1,3,5, ... of this structure
@@ -87,9 +82,7 @@ public:
 
 private:
   void report_error(const std::string &context) {
-    if (working)
-      std::cerr << (context + ": " + std::string(strerror(errno))) << std::endl;
-    working = false;
+    throw std::runtime_error(context + ": " + std::string(strerror(errno)));
   }
 };
 #endif
