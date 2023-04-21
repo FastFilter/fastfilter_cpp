9f11b8d2093ee0b91fab270c8dfc4066feab1e7f
Daniel Lemire <daniel@lemire.me>
2023-04-21 13:19:04 -0400

    Cleaning.

--- benchmarks/linux-perf-events.h
+++ benchmarks/linux-perf-events.h
@@ -1,4 +1,3 @@
-// https://github.com/WojciechMula/toys/blob/master/000helpers/linux-perf-events.h
 #pragma once
 #ifdef __linux__
 
@@ -6,23 +5,24 @@
 #include <linux/perf_event.h> // for perf event constants
 #include <sys/ioctl.h>        // for ioctl
 #include <unistd.h>           // for syscall
-#include <iostream>
+
 #include <cerrno>  // for errno
 #include <cstring> // for memset
 #include <stdexcept>
 
+#include <iostream>
 #include <vector>
 
 template <int TYPE = PERF_TYPE_HARDWARE> class LinuxEvents {
-  int group;
+  int fd;
   bool working;
-  perf_event_attr attribs;
-  int num_events;
-  std::vector<uint64_t> temp_result_vec;
-  std::vector<int> fds;
+  perf_event_attr attribs{};
+  size_t num_events{};
+  std::vector<uint64_t> temp_result_vec{};
+  std::vector<uint64_t> ids{};
 
 public:
-  explicit LinuxEvents(std::vector<int> config_vec) : group(-1), working(true) {
+  explicit LinuxEvents(std::vector<int> config_vec) : fd(0), working(true) {
     memset(&attribs, 0, sizeof(attribs));
     attribs.type = TYPE;
     attribs.size = sizeof(attribs);
@@ -36,58 +36,69 @@ public:
     const int cpu = -1; // all CPUs
     const unsigned long flags = 0;
 
+    int group = -1; // no group
     num_events = config_vec.size();
+    ids.resize(config_vec.size());
+    uint32_t i = 0;
     for (auto config : config_vec) {
       attribs.config = config;
-      int fd = syscall(__NR_perf_event_open, &attribs, pid, cpu, group, flags);
-      if (fd == -1) {
+      int _fd = static_cast<int>(syscall(__NR_perf_event_open, &attribs, pid, cpu, group, flags));
+      if (_fd == -1) {
         report_error("perf_event_open");
       }
-
-      fds.push_back(fd);
+      ioctl(_fd, PERF_EVENT_IOC_ID, &ids[i++]);
       if (group == -1) {
-        group = fd;
+        group = _fd;
+        fd = _fd;
       }
     }
 
     temp_result_vec.resize(num_events * 2 + 1);
   }
 
-  ~LinuxEvents() {
-    for (auto fd : fds) {
-      close(fd);
-    }
-  }
+  ~LinuxEvents() { if (fd != -1) { close(fd); } }
 
   inline void start() {
-    if (ioctl(group, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP) == -1) {
-      report_error("ioctl(PERF_EVENT_IOC_RESET)");
-    }
+    if (fd != -1) {
+      if (ioctl(fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP) == -1) {
+        report_error("ioctl(PERF_EVENT_IOC_RESET)");
+      }
 
-    if (ioctl(group, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP) == -1) {
-      report_error("ioctl(PERF_EVENT_IOC_ENABLE)");
+      if (ioctl(fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP) == -1) {
+        report_error("ioctl(PERF_EVENT_IOC_ENABLE)");
+      }
     }
   }
 
   inline void end(std::vector<unsigned long long> &results) {
-    if (ioctl(group, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP) == -1) {
-      report_error("ioctl(PERF_EVENT_IOC_DISABLE)");
-    }
+    if (fd != -1) {
+      if (ioctl(fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP) == -1) {
+        report_error("ioctl(PERF_EVENT_IOC_DISABLE)");
+      }
 
-    if (read(group, &temp_result_vec[0], temp_result_vec.size() * 8) == -1) {
-      report_error("read");
+      if (read(fd, temp_result_vec.data(), temp_result_vec.size() * 8) == -1) {
+        report_error("read");
+      }
     }
     // our actual results are in slots 1,3,5, ... of this structure
-    // we really should be checking our ids obtained earlier to be safe
     for (uint32_t i = 1; i < temp_result_vec.size(); i += 2) {
       results[i / 2] = temp_result_vec[i];
     }
+    for (uint32_t i = 2; i < temp_result_vec.size(); i += 2) {
+      if(ids[i/2-1] != temp_result_vec[i]) {
+        report_error("event mismatch");
+      }
+    }
+
+
+  }
+
+  bool is_working() {
+    return working;
   }
 
 private:
-  void report_error(const std::string &context) {
-    if (working)
-      std::cerr << (context + ": " + std::string(strerror(errno))) << std::endl;
+  void report_error(const std::string &) {
     working = false;
   }
 };
